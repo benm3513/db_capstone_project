ad6c3f6349c0a55192fa2967065464b61aa0a44b
benm3513 <benm3513@gmail.com>
2023-10-16 12:21:17 -0700

    Week 2 added stored procedures, views, prepared statements

---
+++ littlelemon2.sql
@@ -0,0 +1,273 @@
+-- MySQL dump 10.13  Distrib 8.0.24, for macos11 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dblittlelemon
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Customers`
+--
+
+DROP TABLE IF EXISTS `Customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Customers` (
+  `CustomerId` int NOT NULL,
+  `CustomerName` varchar(45) DEFAULT NULL,
+  `CustomerContact` int DEFAULT NULL,
+  PRIMARY KEY (`CustomerId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Customers`
+--
+
+LOCK TABLES `Customers` WRITE;
+/*!40000 ALTER TABLE `Customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Delivery`
+--
+
+DROP TABLE IF EXISTS `Delivery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Delivery` (
+  `DeliveryID` int NOT NULL,
+  `DeliveryStatus` varchar(45) DEFAULT NULL,
+  `DeliveryDate` date DEFAULT NULL,
+  PRIMARY KEY (`DeliveryID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Delivery`
+--
+
+LOCK TABLES `Delivery` WRITE;
+/*!40000 ALTER TABLE `Delivery` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Delivery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Menu`
+--
+
+DROP TABLE IF EXISTS `Menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Menu` (
+  `MenuID` int NOT NULL,
+  `MenuName` varchar(45) DEFAULT NULL,
+  `MenuItemsID` int DEFAULT NULL,
+  `Cuisine` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`MenuID`),
+  KEY `MenuItemsID_idx` (`MenuItemsID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Menu`
+--
+
+LOCK TABLES `Menu` WRITE;
+/*!40000 ALTER TABLE `Menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `MenusItems`
+--
+
+DROP TABLE IF EXISTS `MenusItems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `MenusItems` (
+  `menu_item_id` int NOT NULL,
+  `menu_id` int DEFAULT NULL,
+  `CourseName` varchar(45) DEFAULT NULL,
+  `StarterName` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`menu_item_id`),
+  UNIQUE KEY `menu_item_id_UNIQUE` (`menu_item_id`),
+  KEY `menu_id_idx` (`menu_id`),
+  CONSTRAINT `menu_id` FOREIGN KEY (`menu_id`) REFERENCES `Menu` (`MenuID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `MenusItems`
+--
+
+LOCK TABLES `MenusItems` WRITE;
+/*!40000 ALTER TABLE `MenusItems` DISABLE KEYS */;
+/*!40000 ALTER TABLE `MenusItems` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Orders`
+--
+
+DROP TABLE IF EXISTS `Orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Orders` (
+  `OrderId` int NOT NULL,
+  `OrderDate` date DEFAULT NULL,
+  `OrderQuantity` int DEFAULT NULL,
+  `TotalCost` decimal(10,0) DEFAULT NULL,
+  `DeliveryID` int DEFAULT NULL,
+  `CustomerID` int DEFAULT NULL,
+  `MenuID` int DEFAULT NULL,
+  PRIMARY KEY (`OrderId`),
+  KEY `DeliveryID_idx` (`DeliveryID`),
+  KEY `CustomerID_idx` (`CustomerID`),
+  KEY `MenuID_idx` (`MenuID`),
+  CONSTRAINT `CustomerId` FOREIGN KEY (`CustomerID`) REFERENCES `Customers` (`CustomerId`),
+  CONSTRAINT `DeliveryID` FOREIGN KEY (`DeliveryID`) REFERENCES `Delivery` (`DeliveryID`),
+  CONSTRAINT `MenuID` FOREIGN KEY (`MenuID`) REFERENCES `Menu` (`MenuID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Orders`
+--
+
+LOCK TABLES `Orders` WRITE;
+/*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `ordersview`
+--
+
+DROP TABLE IF EXISTS `ordersview`;
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `ordersview` AS SELECT 
+ 1 AS `OrderID`,
+ 1 AS `OrderQuantity`,
+ 1 AS `TotalCost`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `Staff`
+--
+
+DROP TABLE IF EXISTS `Staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Staff` (
+  `StaffID` int NOT NULL,
+  `StaffName` varchar(45) DEFAULT NULL,
+  `StaffRole` varchar(45) DEFAULT NULL,
+  `StaffSalary` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`StaffID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Staff`
+--
+
+LOCK TABLES `Staff` WRITE;
+/*!40000 ALTER TABLE `Staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'dblittlelemon'
+--
+
+--
+-- Dumping routines for database 'dblittlelemon'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `CancelOrder` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`capstone`@`%` PROCEDURE `CancelOrder`(IN orderIdToDelete INT)
+BEGIN
+    DECLARE orderExists INT;
+    SELECT COUNT(*) INTO orderExists FROM Orders WHERE OrderID = orderIdToDelete;
+    IF orderExists > 0 THEN
+        DELETE FROM Orders WHERE OrderID = orderIdToDelete;
+        SELECT CONCAT('Order ID ', orderIdToDelete, ' has been canceled.') AS Result;
+    ELSE
+        SELECT 'Order not found. No action taken.' AS Result;
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `GetMaxQuantity` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`capstone`@`%` PROCEDURE `GetMaxQuantity`()
+BEGIN
+    SELECT MAX(quantity) AS max_quantity_in_order FROM Orders;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `ordersview`
+--
+
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`capstone`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `ordersview` AS select `orders`.`OrderId` AS `OrderID`,`orders`.`OrderQuantity` AS `OrderQuantity`,`orders`.`TotalCost` AS `TotalCost` from `orders` where (`orders`.`OrderQuantity` > 2) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-16 12:05:53
